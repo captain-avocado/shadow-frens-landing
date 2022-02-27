f77a840d1b4700ff9236efd616c9e7aa41c52989
Artem Kaloev <kaloev@hypeauditor.com>
2022-02-27 15:47:53 +0300

    Layout fixes

--- components/Intro.vue
+++ components/Intro.vue
@@ -2,10 +2,19 @@
 .intro.flex.justify-center
   Container
     .intro__content
-      .intro__cards.mb-56
-        ImageCommon.intro__card.mr-40(src="/intro_4.png")
-        ImageCommon.intro__card.--center(src="/intro_1.png")
-        ImageCommon.intro__card.ml-40(src="/intro_8.jpg")
+      .intro__cards.mb-160
+        ImageCommon.intro__card(
+          src="/intro_4.png"
+          :class="{'--left': classes[0]===0,'--right': classes[0]===2,'--center': classes[0]===1 }"
+        )
+        ImageCommon.intro__card(
+          src="/intro_1.png"
+          :class="{'--left': classes[0]===1,'--right': classes[0]===0,'--center': classes[0]===2 }"
+        )
+        ImageCommon.intro__card(
+          src="/intro_8.jpg"
+          :class="{'--left': classes[0]===2,'--right': classes[0]===1,'--center': classes[0]===0 }"
+        )
       Title.intro__title Shadow Frens
       .intro__text.mb-32 NFT Collection on Solana combining unique utilities and original art with 200 hand-drawn traits
       .intro__button.flex.items-center.justify-center
@@ -14,39 +23,59 @@
 </template>
 
 <script>
+// import { sleep } from '@/sleep'
+
 export default {
   name: 'IntroSection',
   data () {
     return {
-      images: [
-        '/intro_4.png',
-        '/intro_1.png',
-        '/intro_8.png'
-      ]
+      classes: [0, 1, 2]
     }
   }
+  // async mounted () {
+  //   for (let i = 0; i < 10; i++) {
+  //     await sleep(2000)
+  //     this.classes = this.classes.map(index => index === 2 ? 0 : index + 1)
+  //   }
+  // }
 }
 </script>
 
 <style lang="scss" scoped>
 .intro {
   &__content {
-    padding: 120px 0;
+    padding: 200px 0;
     width: 100%;
     max-width: 520px;
     text-align: center;
   }
 
+  &__cards {
+    position: relative;
+  }
+
   &__card {
     width: 200px;
     height: 200px;
+    transition: transform .5s ease-in-out;
+    position: absolute;
+
+    &.--left {
+      top: 50%;
+      transform: translate(-120%, -50%) scale(1);
+    }
+
+    &.--right {
+      top: 50%;
+      transform: translate(20%, -50%) scale(1);
+    }
 
     &.--center {
-      position: absolute;
-      width: 250px;
-      height: 250px;
       z-index: 1;
-      transform: translate(-50%, -30px);
+      left: 50%;
+      top: -100px;
+      transform-origin: top;
+      transform: translate(-50%, -30px) scale(1.25);
     }
   }
 }
--- components/Logo.vue
+++ components/Logo.vue
@@ -1,11 +1,14 @@
 <template lang="pug">
-  a(href="#")
+  a(:href="$options.TWITTER")
     img.logo(src="/logo.svg")
 </template>
 
 <script>
+import { TWITTER_ACCOUNT_LINK } from '@/configs/constants'
+
 export default {
-  name: 'UILogo'
+  name: 'UILogo',
+  TWITTER: TWITTER_ACCOUNT_LINK
 }
 </script>
 
