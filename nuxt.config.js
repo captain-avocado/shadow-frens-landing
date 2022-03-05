016addf829b987f14d96935f91b410edb742ff95
Artem Kaloev <kaloev@hypeauditor.com>
2022-03-05 04:13:52 +0300

    Update metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'NFT Collection on Solana combining unique utilities and original art with 200 hand-drawn traits' },
+      { hid: 'og:image', property: 'og:image', content: './og.png' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
